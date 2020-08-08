928a96e92cfb610b9cee92538440269ec63491db
Sharl Sherif <dev.sharl.sherif@gmail.com>
2020-08-09 00:34:25 +0200

    final working version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
-chrome.runtime.onMessage.addListener(function(width, sender, sendResponse) {
-    GenerateBorders(width)
-    sendResponse('thanks!')
+chrome.runtime.onMessage.addListener(function (payload, sender, sendResponse) {
+  if (payload.type == "widthValue") {
+    if (localStorage.toggleState !== undefined) {
+      if (JSON.parse(localStorage.toggleState) == false) {
+        sendResponse("Extension is off");
+        return;
+      }
+
+      if (JSON.parse(localStorage.toggleState) == true) {
+        console.log("main.js toggleState", localStorage.toggleState);
+        GenerateBorders(payload.data);
+      }
+    } else {
+      console.log("Toggle state was never defined before on this page");
+      GenerateBorders(0.00000001);
+    }
+  } else if (payload.type == "toggleState") {
+    console.log("I should remove or add borders", payload.data);
+    // extension turned off
+    if (payload.data == false) {
+      window.location.reload();
+    } else {
+      // extension getting turned on
+      GenerateBorders(localStorage.borderWidth);
+    }
+  }
+  StoreInLocalStorage(payload.type, payload.data);
+  sendResponse("thanks!");
 });
 
-function GenerateBorders(width) {
-    const PageElements = document.querySelectorAll("*");
-    for (let element of PageElements) {
-        const generateColor = randomColor({count: 1});
-
-        element.style['border'] = `${width}px solid ${generateColor}`;
-    }
+// generate the stored value for that specific webpage
+if (
+  !!localStorage.toggleState &&
+  JSON.parse(localStorage.toggleState) == true
+) {
+  if (!!localStorage.borderWidth) {
+    GenerateBorders(localStorage.borderWidth);
+  } else {
+    GenerateBorders(0.00000001);
+  }
 }
+
+function GenerateBorders(width) {
+  const PageElements = document.querySelectorAll("*");
+  for (let element of PageElements) {
+    const generateColor = randomColor({ count: 1 });
+
+    element.style["border"] = `${width}px solid ${generateColor}`;
+  }
+}
+
+function StoreInLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+console.log(localStorage);
+// GenerateBorders(1)
--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,107 @@
-let currTabId
+let currTabId;
 
 chrome.tabs.onActivated.addListener((tabId, changeInfo, tab) => {
-    console.log('new window opened')
-    chrome.tabs.get(tabId.tabId, function (tab) {
-        currTabId = tab.id
-    })
+  console.log("new window opened");
+  chrome.tabs.get(tabId.tabId, function (tab) {
+    currTabId = tab.id;
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function load(event) {
-    const createButton = document.querySelector('#genColor');
+document.addEventListener("DOMContentLoaded", async function load(event) {
+  if (localStorage.toggleState == undefined) {
+    console.log("init toggle button");
+    await initToggleButtonFirstTime();
+  } else {
+    initToggleButtonAlwaysRun();
+  }
 
-    createButton.addEventListener('change', () => buttonClicked());
+  const form = document.querySelector(".input-container");
+  const toggleButton = document.querySelector(".toggle-button");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    applyButtonClicked();
+  });
+  toggleButton.addEventListener("click", () => toggleButtonClicked());
+
+  if (!!localStorage.borderWidth) {
+    document.querySelector(".border-width").value = localStorage.borderWidth;
+    applyButtonClicked();
+  }
 });
 
-async function buttonClicked() {
-    const widthValue = document.querySelector('#genColor').value;
-    await chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        currTabId = tabs[0].id
-        console.log(currTabId)
-        chrome.tabs.sendMessage(currTabId, widthValue, function (response) {
-            console.log(response)
-        });
+async function toggleButtonClicked() {
+  let currState = JSON.parse(localStorage.toggleState);
+  if (currState == true) {
+    // turn off
+    StoreInLocalStorage("toggleState", false);
+    currState = false;
+    document.querySelector(".toggle-button").src = "assets/red-button.svg";
+  } else {
+    StoreInLocalStorage("toggleState", true);
+    currState = true;
+    document.querySelector(".toggle-button").src = "assets/green-button.svg";
+  }
+
+  const currTabId = await getCurrentTabId();
+  const payload = { data: currState, type: "toggleState" };
+
+  chrome.tabs.sendMessage(currTabId, payload, function (response) {
+    console.log(response);
+  });
+}
+
+async function applyButtonClicked() {
+  const width = document.querySelector(".border-width").value;
+  const currTabId = await getCurrentTabId();
+  const payload = { data: width, type: "widthValue" };
+  StoreInLocalStorage("borderWidth", width);
+
+  chrome.tabs.sendMessage(currTabId, payload, async function (response) {
+    console.log(response);
+    if (response == "Extension is off") {
+      await notify("Settings are saved, but the extension is turned off!");
+    }
+  });
+}
+function initToggleButtonAlwaysRun() {
+  let currState = JSON.parse(localStorage.toggleState);
+  if (currState == true) {
+    document.querySelector(".toggle-button").src = "assets/green-button.svg";
+  } else {
+    document.querySelector(".toggle-button").src = "assets/red-button.svg";
+  }
+}
+async function initToggleButtonFirstTime() {
+  const currTabId = await getCurrentTabId();
+  const payload = { data: true, type: "toggleState" };
+  StoreInLocalStorage("toggleState", true);
+  document.querySelector(".toggle-button").src = "assets/green-button.svg";
+
+  chrome.tabs.sendMessage(currTabId, payload, function (response) {
+    console.log(response);
+  });
+}
+
+// helpers
+async function getCurrentTabId() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      currTabId = tabs[0].id;
+      console.log(currTabId);
+      resolve(currTabId);
     });
+  });
+}
+
+function StoreInLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+async function notify(value) {
+  document.querySelector(".message").innerText = value;
+  document.querySelector(".notification").style.display = "block";
+
+  setTimeout(() => {
+    document.querySelector(".notification").style.display = "none";
+  }, 3000);
 }
